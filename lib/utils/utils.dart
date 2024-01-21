b18c5d24f830c1a7def5d81565091eba64ea8be2
molgeek <74042620+morgueye4@users.noreply.github.com>
2024-01-21 18:39:33 +0000

    Connect to rpc1.relai.network

--- lib/utils/utils.dart
+++ lib/utils/utils.dart
@@ -4,17 +4,12 @@ import 'package:polkadart/polkadart.dart' show WsProvider, StateApi;
 class Utils {
   static void testConnection() async {
     try {
-      // final relai_network = WsProvider(Uri.parse(RELAI_RPC));
-      // await relai_network.connect();
-      // print('✅ Connected to the Substrate node!');
-      // You can now use the wsProvider to interact with the node
-
-      final polkadot = WsProvider(Uri.parse(RELAI_RPC));
-      final api = StateApi(polkadot);
+      final relai_network = WsProvider(Uri.parse(RELAI_RPC));
+      final api = StateApi(relai_network);
       final runtimeVersion = await api.getRuntimeVersion();
       print(runtimeVersion.toJson());
       print('✅ Connected to the Substrate node!');
-      await polkadot.disconnect();
+      await relai_network.disconnect();
 
     } catch (e) {
       print('❌ Failed to connect to the Substrate node: $e');
